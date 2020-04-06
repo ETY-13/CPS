8e563fbdaafea8a6ec0de25ef854b75be805f035
chale11 <chale11@alaska.edu>
2020-04-06 00:12:36 -0800

    added more tests in main

--- main.cpp
+++ main.cpp
@@ -16,38 +16,50 @@ using std::filebuf;
 #include<iostream>
 using std::ios;
 
-// out put shape to file 
+// out put shape to file
 void getShapetoFile() {
     filebuf outfile;
     outfile.open("shape.ps", ios::out);
 
     ostream os(&outfile);
-    auto cirlce = makeCircle(4);
-    auto poly = makePolygon(8, 10);
+    auto circle = makeCircle(4);
+    auto poly = makePolygon(6, 50);
+    auto poly2 = makePolygon(8, 75.5);
+    auto poly3 = makePolygon(5, 50);
     auto rect = makeRectangle(20, 20);
+    auto tria = makeTriangle(45);
+    //auto triaRot = makeRotatedShape(tria,); idk how to make an angle
     os << "% circle\n";
-    cirlce->generatePostScript(os);
+    circle->generatePostScript(os);
     os << "% polygon\n";
     poly->generatePostScript(os);
+    os << "% polygon 2\n";
+    poly2->generatePostScript(os);
+    os << "% polygon 3\n";
+    poly3->generatePostScript(os);
     os << "%rectangle\n";
     rect->generatePostScript(os);
+    os << "%triangle\n";
+    tria->generatePostScript(os);
+    //os << "%Rotated triangle\n";
+    //triaRot->generatePostScript(os);
     outfile.close();
 
 }
 
 int main(int argc, char* argv[]) {
 
-    Catch::Session session;  
-    int result;         
+    Catch::Session session;
+    int result;
 
     result = session.applyCommandLine(argc, argv);
 
     if (result !=0){
         return result;
     }
-    
+
     result = session.run();
-   
+
     getShapetoFile();
 
     return result;
